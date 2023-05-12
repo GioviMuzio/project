2a42c64e98bbc54590752d6075c113c7d2fc3994
Lasa <102794245+Devilasa@users.noreply.github.com>
2023-05-12 18:20:01 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,10 @@
 public class main {
+
     public static void main(String[] args) {
         System.out.println("Frappuzziner");
+        System.out.println("Ciao brozi");
+        for(;;){
+            System.out.println("Raccoon");
+        }
     }
 }
