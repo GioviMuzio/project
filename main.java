39bf56c07453a92bb2d4eb68795d0d1d7eda624e
GioviMuzio <133395600+GioviMuzio@users.noreply.github.com>
2023-05-12 18:38:13 +0200

    Update main.java

--- main.java
+++ main.java
@@ -2,9 +2,10 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("Frappuzziner");
-        System.out.println("Ciao brozi");
+        System.out.println("Ciao brozhttps://github.com/Devilasa/projecti");
         for(;;){
             System.out.println("Raccoon");
         }
+        ehehjfhea
     }
 }
